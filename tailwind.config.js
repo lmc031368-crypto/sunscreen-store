66819287baddd74d9d0759c7cc6a76455e6a8d05
cm l <lmc031368@gmail.com>
2026-04-30 18:00:15 +0800

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
